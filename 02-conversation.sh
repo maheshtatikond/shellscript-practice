e7133ff90e33b0ba06b61adf0c32473b9706ac4e
SAI MAHESH TATIKONDA <saimaheshtatikonda@SAIs-MacBook-Pro.local>
2026-02-03 19:47:47 +1100

    third edit

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,3 +3,8 @@
 echo "Sai:How are you?"
 echo " Mahesh: I am fine, thank you!"
 echo "I am practicing shell scripting."
+echo "Sai is teaching me."
+echo "Mahesh: That's great!"
+echo "Sai: Yes, it is!"
+echo "Mahesh: Let's continue learning."
+
