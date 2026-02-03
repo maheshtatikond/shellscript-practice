681bafe010a0ed0a15d860c26f4ae9be1d27c611
SAI MAHESH TATIKONDA <saimaheshtatikonda@SAIs-MacBook-Pro.local>
2026-02-03 19:59:44 +1100

    created varaiblest

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+Person1=Sai
+Person2=Mahesh
+
+echo "$Person1:How are you?"
+echo " $Person2: I am fine, thank you!"
+echo "I am practicing shell scripting."
+echo "$Person1 is teaching me."
+echo "$Person2: That's great!"
+echo "$Person1: Yes, it is!"
+echo "$Person2: Let's continue learning."
