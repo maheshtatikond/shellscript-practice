1375c8b9ed4d9c5c86d613e4b8e1f835f7f3a672
SAI MAHESH TATIKONDA <saimaheshtatikonda@SAIs-MacBook-Pro.local>
2026-02-03 20:25:36 +1100

    assigned argu

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:How are you?"
+echo " $Person2: I am fine, thank you!"
+echo "I am practicing shell scripting."
+echo "$Person1 is teaching me."
+echo "$Person2: That's great!"
+echo "$Person1: Yes, it is!"
+echo "$Person2: Let's continue learning."
+
